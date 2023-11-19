497ac5a2938d195b958a1fd42368570c2505a901
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-11-19 17:52:37 +0200

    initial commit

---
+++ AlbumApp_Django/web/urls.py
@@ -0,0 +1,12 @@
+
+
+urlpatterns=("")
+#
+#
+# · http://localhost:8000/ - home page
+# · http://localhost:8000/album/add/ - add album page
+# · http://localhost:8000/album/details/<id>/ - album details page
+# · http://localhost:8000/album/edit/<id>/ - edit album page
+# · http://localhost:8000/album/delete/<id>/ - delete album page
+# · http://localhost:8000/profile/details/ - profile details page
+# · http://localhost:8000/profile/delete/ - delete profile page
